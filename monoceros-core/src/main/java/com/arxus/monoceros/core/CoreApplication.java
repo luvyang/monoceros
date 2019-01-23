719dfa004877c90ef09a4900ea969b64f2ceea50
luvyang <mylove1621@qq.com>
2019-01-23 12:50:55 +0800

    Build Project

---
+++ monoceros-core/src/main/java/com/arxus/monoceros/core/CoreApplication.java
@@ -0,0 +1,14 @@
+package com.arxus.monoceros.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreApplication.class, args);
+    }
+
+}
+
---
+++ monoceros-system/src/main/java/com/arxus/monoceros/system/SystemApplication.java
@@ -0,0 +1,14 @@
+package com.arxus.monoceros.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SystemApplication.class, args);
+    }
+
+}
+
