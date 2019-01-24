144ef7cb7cc911dd1c7d14995997a04997c3baa6
luvyang <mylove1621@qq.com>
2019-01-24 11:05:41 +0800

    base rebuild

--- monoceros-base/src/main/java/com/arxus/monoceros/base/kit/StringKit.java
+++ monoceros-base/src/main/java/com/arxus/monoceros/base/kit/StringKit.java
@@ -1,5 +1,7 @@
 package com.arxus.monoceros.base.kit;
 
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * 字符串工具类
  *
@@ -8,5 +10,17 @@ package com.arxus.monoceros.base.kit;
  */
 public final class StringKit {
     private StringKit() {
+    }
+
+    /**
+     * 判断字符串是否为空，或全是空格
+     *
+     * @param cs
+     * @return
+     * @Author : YangXuyue
+     * @Date : 2019/1/24 11:02
+     */
+    public static boolean isBlank(CharSequence cs) {
+        return StringUtils.isBlank(cs);
     }
 }
